bb415f3d43de82ba64aff06bd7e5f45077764b71
Thita Pinsri <thitapinsri@gmail.com>
2022-08-21 23:05:38 +0700

    add path condition

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,47 @@
 import "./NavBar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from '/assets/logo.png'
 
 
 const NavBar = () => {
-  return (
-    <div className="navbar">
-      <Link to="/" className="logo">
-        <img src={logo} alt='logo'/>
-        EXTRACKS
-      </Link>
-      <nav>
-        <Link to="/">Home</Link>
+  let location = useLocation();
+
+  let nav
+
+  switch (location.pathname) {
+    case "/":
+      nav = <>
+        <Link to="/signin">Sign In</Link>
+        <Link to="/signup">Get Statrted</Link>
+      </>
+      break;
+    case "/signin":
+      nav = <>
+        <Link to="/signup">Sign Up</Link>
+      </>
+      break;
+    case "/signup":
+      nav = <>
+        <Link to="/signin">Sign In</Link>
+      </>
+      break;
+    default:
+      nav = <>
         <Link to="/activities">My Activity</Link>
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/setting">Setting</Link>
+      </>
+      break;
+  }
+
+  return (
+    <div className="navbar">
+      <Link to="/" className="logo">
+        <img src={logo} alt='logo' />
+        EXTRACKS
+      </Link>
+      <nav>
+        {nav}
       </nav>
     </div>
   );
