593bb50d4861965c1dfd30e8120413d7d412f9c4
Parinchat Juntaworn <j.parinchat@gmail.com>
2022-08-24 17:06:45 +0700

    edit not found

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,13 +1,11 @@
 import { Link, useLocation } from "react-router-dom";
-import './NotFound.css'
 
 const NotFound = () => {
+
   return (
     <div className="notfound">
       <img src="./assets/jaconda-17.gif" alt="" />
-      {/* <button> */}
       <Link to="/user/dashboard">Back to Dashboard</Link>
-      {/* </button> */}
     </div>
   )
 }
