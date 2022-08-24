2c991837a6d2654cb59b1ac72443ee9f2a6b1331
Parinchat Juntaworn <j.parinchat@gmail.com>
2022-08-24 16:47:26 +0700

    create not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,14 @@
-
+import { Link, useLocation } from "react-router-dom";
+import './NotFound.css'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="notfound">
+      <img src="./assets/jaconda-17.gif" alt="" />
+      {/* <button> */}
+      <Link to="/user/dashboard">Back to Dashboard</Link>
+      {/* </button> */}
+    </div>
   )
 }
 
